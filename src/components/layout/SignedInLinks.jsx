7c0a098aa817939f12250bdad890cb7e542db17d
Anuwat Songchumrong <anuwat_song@outlook.com>
2019-07-19 14:33:18 +0700

    display - user profile

--- src/components/layout/SignedInLinks.jsx
+++ src/components/layout/SignedInLinks.jsx
@@ -4,11 +4,14 @@ import { connect } from 'react-redux'
 import { signOut } from './../../store/actions/authActions'
 
 const SignedInLinks = (props) => {
+    const {profile} = props
     return (
         <ul className="right">
             <li><NavLink to='/create'>New Project</NavLink></li>
             <li><a onClick={props.signOut}>Logout</a></li>
-            <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
+            <li><NavLink to='/' className="btn btn-floating pink lighten-1">
+                {profile.initials}    
+            </NavLink></li>
         </ul>
     )
 }
