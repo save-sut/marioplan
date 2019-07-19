213f41e00e24c390235cb727f62f9d42dde56390
Anuwat Songchumrong <anuwat_song@outlook.com>
2019-07-19 10:48:09 +0700

    sync data with firestore - project detail page

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
+import { Link } from 'react-router-dom'
 
 const ProjectList = ({ projects }) => {
     return (
         <div className="project-list section">
             {projects &&
-                projects.map(project => <ProjectSummary key={project.id} project={project} />)}
+                projects.map(project => {
+                    return (
+                        <Link key={project.id} to={'/project/' + project.id}>
+                            <ProjectSummary
+                                key={project.id}
+                                project={project}
+                            />
+                        </Link>
+                    )
+                })}
         </div>
     )
 }
