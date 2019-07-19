670248580eb25035a2ba2c6cf635afa318beecf8
Anuwat Songchumrong <anuwat_song@outlook.com>
2019-07-19 14:11:07 +0700

    firebase auth - user sign up

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -26,3 +26,25 @@ export const signOut = credentials => {
             })
     }
 }
+
+export const signUp = newUser => {
+    return (dispatch, getState, { getFirebase, getFirestore}) => {
+        const firebase = getFirebase()
+        const firestore = getFirestore()
+
+        firebase.auth().createUserWithEmailAndPassword(
+            newUser.email,
+            newUser.password
+        ).then((response) => {
+            return firestore.collection('user').doc(response.user.uid).set({
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                initials: newUser.firstName[0] + newUser.lastName[0]
+            })
+        }).then(() => {
+            dispatch({ type: 'SIGNUP_SUCCESS' })
+        }).catch((err) => {
+            dispatch({ type: 'SIGNUP_ERROR', err })
+        })
+    }
+}
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -14,7 +14,17 @@ const authReducer = (state = initState, action) => {
 
         case 'LOGOUT_SUCCESS': {
             console.log('logout success');
-            return state
+            return { ...state, authError: 'null' }
+        }
+
+        case 'SIGNUP_ERROR': {
+            console.log('signup error');
+            return { ...state, authError: action.err.message }
+        }
+
+        case 'SIGNUP_SUCCESS': {
+            console.log('signup success');
+            return { ...state, authError: null }
         }
 
         default:
