39808a8291a6f2ba54fb71da310768a68207187e
Anuwat Songchumrong <anuwat_song@outlook.com>
2019-07-19 13:36:45 +0700

    route guarding

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -4,34 +4,46 @@ import ProjectList from './../projects/ProjectList'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import { Redirect } from 'react-router-dom'
 
 class Dashboard extends Component {
     render() {
-        const { projects } = this.props
-        return (
-            <div className="dashboard container">
-                <div className="row">
-                    <div className="col s12 m6">
-                        <ProjectList projects={projects} />
-                    </div>
-                    <div className="col s12 m5 offset-m1">
-                        <Notifications />
+        const { projects, auth } = this.props
+        if (!auth.uid) {
+            return <Redirect to="/signin" />
+        }
+
+        if (projects) {
+            return (
+                <div className="dashboard container">
+                    <div className="row">
+                        <div className="col s12 m6">
+                            <ProjectList projects={projects} />
+                        </div>
+                        <div className="col s12 m5 offset-m1">
+                            <Notifications />
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div className="container center">
+                    <p>Loading Dashboard...</p>
+                </div>
+            )
+        }
     }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     return {
-        projects: state.firestore.ordered.projects
+        projects: state.firestore.ordered.projects,
+        auth: state.firebase.auth
     }
 }
 
 export default compose(
     connect(mapStateToProps),
-    firestoreConnect([
-        { collection: 'projects' }
-    ])
+    firestoreConnect([{ collection: 'projects' }])
 )(Dashboard)
